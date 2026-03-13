ac1fa5e643c308d24e379dee587e1fb2b83bc99d
IvanNIvanovich <153431289+IvanNIvanovich@users.noreply.github.com>
2026-03-13 15:12:52 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        /*P1.6 (Red / Червоний), P0.2 (Green / Зелений) та P0.3 (Blue / Синій),*/
+        if(Button_Read() == 1)
+        {
+            LED_Write(0);
+        }
+        else
+        {
+            LED_Write(1);
+        }
+        
+    }
+}
+
+/* [] END OF FILE */
